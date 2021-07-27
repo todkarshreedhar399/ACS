ea303cbf31aff751978deeac85392ffd0fb6d4aa
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-07-27 18:47:14 +0530

    Latest Update 7

--- acoperations.sh
+++ acoperations.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -x
 declare -A dict
 read -p  "Enter the number of records:" num
+counter=0
 for ((i=0;i<$num;i++))
 do
 read -p "Enter the type of value: " key
 read -p "Enter the value: " value
 dict["$key"]="$value"
+a[((counter++))]="${dict["$key"]}"
 done
 echo "${dict[@]}"
+echo "${a[@]}"
