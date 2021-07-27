e3d8ee46b22d27ee4d82c48c5ecf4546ba0e9243
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-07-27 19:28:40 +0530

    Update 6

--- acoperations.sh
+++ acoperations.sh
@@ -1,10 +1,33 @@
 #!/bin/bash -x
-declare -A dict
-read -p  "Enter the number of records:" num
-for ((i=0;i<$num;i++))
-do
-read -p "Enter the type of value: " key
-read -p "Enter the value: " value
-dict["$key"]="$value"
-done
-echo "${dict[@]}"
+
+declare -A computeDictionary
+
+read -p "Enter a value: " a
+read -p "Enter b value: " b
+read -p "Enter c value: " c
+
+echo "a= $a"
+echo "b= $b"
+echo "c= $c"
+
+temp1=$(($a+$b))
+compute1=$(($temp1*$c))
+echo "answer: $compute1"
+
+temp2=$(($a*$b))
+compute2=$(($temp2+$c))
+echo "answer: $compute2"
+
+temp3=$(($c+$a))
+compute3=$(($temp3/$b))
+echo "answer: $compute3"
+
+temp4=$(($a%$b))
+compute4=$(($temp4+$c))
+echo "answer: $compute4"
+
+computeDictionary[eq1]=[$compute1]
+computeDictionary[eq2]=[$compute2]
+computeDictionary[eq3]=[$compute3]
+computeDictionary[eq4]=[$compute4]
+echo "${computeDictionary[*]}"
