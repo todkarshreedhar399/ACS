5123142bd3271fd82aa20f05aadbe149e8d1a9cf
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-07-27 21:09:43 +0530

    Update 9

--- acoperations.sh
+++ acoperations.sh
@@ -1,13 +1,52 @@
 #!/bin/bash -x
-declare -A dict
-read -p  "Enter the number of records:" num
+
+declare -A computeDictionary
+
+read -p "Enter a value: " a
+read -p "Enter b value: " b
+read -p "Enter c value: " c
 counter=0
-for ((i=0;i<$num;i++))
+
+echo "a= $a"
+echo "b= $b"
+echo "c= $c"
+
+temp1=$(($a+$b))
+compute1=$(($temp1*$c))
+echo "answer: $compute1"
+
+temp2=$(($a*$b))
+compute2=$(($temp2+$c))
+echo "answer: $compute2"
+
+temp3=$(($c+$a))
+compute3=$(($temp3/$b))
+echo "answer: $compute3"
+
+temp4=$(($a%$b))
+compute4=$(($temp4+$c))
+echo "answer: $compute4"
+
+Array[((counter++))]=$compute1
+Array[((counter++))]=$compute2
+Array[((counter++))]=$compute3
+Array[((counter++))]=$compute4
+echo "${Array[*]}"
+
+
+for ((i=0;i<4;i++))
 do
-read -p "Enter the type of value: " key
-read -p "Enter the value: " value
-dict["$key"]="$value"
-a[((counter++))]="${dict["$key"]}"
-done
-echo "${dict[@]}"
-echo "${a[@]}"
+	for ((j=i+1;j<4;j++))
+	do
+		if [ ${Array[j]} -lt ${Array[i]} ]
+		then
+			temp=$[${Array[i]}]
+			Array[i]=$[${Array[j]}]
+			Array[j]=$[$temp]
+		fi
+		done
+	done
+
+
+echo "Ascending order of an array: ${Array[*]}"
+
