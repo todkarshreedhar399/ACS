638684b51a5f25d83bc2061c771aae9c14da018a
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-07-27 17:01:10 +0530

    Update 5

--- acoperations.sh
+++ acoperations.sh
@@ -2,5 +2,5 @@
 read -p "Enter number a:" a
 read -p "Enter number b:" b
 read -p "Enter number c:" c
-sum=$[$c+$a/$b]
-echo "Computation(c+a/b)=$sum"
+sum=$[$a%$b+$c]
+echo "Computation(a%b+c)=$sum"
